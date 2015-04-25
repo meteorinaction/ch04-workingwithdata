949c0e6ff29fc06f9bd77e70ba6bf4c83005586c
Stephan Hochhaus <stephan@yauh.de>
2015-04-25 18:40:39 +0200

    Listing 4.19 Event map for deleting a house

--- houseSitter/client/client.js
+++ houseSitter/client/client.js
@@ -30,6 +30,15 @@ Template.showHouse.helpers({
     });
   }
 });
+Template.showHouse.events({
+  'click button#delete': function (evt) {
+    var id = this._id;
+    var deleteConfirmation = confirm("Really delete this house?");
+    if (deleteConfirmation) {
+      HousesCollection.remove(id);
+    }
+  }
+});
 
 Template.plantDetails.helpers({
   isWatered: function () {
